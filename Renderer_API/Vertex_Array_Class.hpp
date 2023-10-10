df79b54ace10ca619777e8b7b44b5ca3dd75cda0
srikargridpro <147362350+srikargridpro@users.noreply.github.com>
2023-10-10 12:25:08 +0530

    Update Vertex_Array_Class.hpp

--- Renderer_API/Vertex_Array_Class.hpp
+++ Renderer_API/Vertex_Array_Class.hpp
@@ -17,13 +17,26 @@ float position[3];
 float color[3];
 float normal[3];
 float* getPosition() { return this->position;}
-float* getColor() { return this->color;}
-float* getNormal() { return this->normal;}
+float* getColor()    { return this->color;   }
+float* getNormal()   { return this->normal;  }
 };
 
 Vertex operator[](int index)
 {
- 
+ uint32_t n = (this->layout > 1) ? (this->layout > 3 ? 3 : 2) : 1 ;
+ uint32_t stride_len = n*3;
+ Vertex vertex;
+ for(size_t i = 0 ; i < 3 ; ++i)
+ {
+   vertex.position[i] = this->vertex_array[index*stride_len + i];
+   if(n == 2 || n == 4)
+   vertex.color[i]    = this->vertex_array[index*stride_len + 3 + i]; // color offset = 3
+   if(n == 3)
+   vertex.normal[i]   = this->vertex_array[index*stride_len + 3 + i]; // normal offset = 3
+   if(n == 4)
+   vertex.normal[i]   = this->vertex_array[index*stride_len + 6 + i]; // normal offset = 6
+ }
+ return vertex;
 }
 
 bool setLayout(AttribLayout input_layout)  {  this->layout = input_layout ; return (input_layout > 0 ? true : false); }

8eef58eb0fdb0c56c12c40db233bae4a9ee4b26d
srikargridpro <147362350+srikargridpro@users.noreply.github.com>
2023-10-10 18:21:21 +0530

    Create Model_Class.hpp

---
+++ Renderer_API/Model_Class.hpp
@@ -0,0 +1,21 @@
+#ifndef _MODEL_CLASS_HPP_
+#define _MODEL_CLASS_HPP_
+#define USE_HEADER_ONLY_IMPLEMENTATION
+#include "Vertex_Array_Class.hpp"
+
+namespace Renderer_API {
+class Model {
+public :
+
+uint32_t VBO, VAO;
+VertexArray vertex_array; 
+Shader ModelShader;
+
+
+};
+
+
+
+
+} // namespace Renderer_API
+#endif // _MODEL_CLASS_HPP_

a7dc9cdf584f661c9333b0f2d880d8dc052d2fdc
srikargridpro <147362350+srikargridpro@users.noreply.github.com>
2023-10-10 11:08:07 +0530

    Update Vertex_Array_Class.hpp

--- Renderer_API/Vertex_Array_Class.hpp
+++ Renderer_API/Vertex_Array_Class.hpp
@@ -1,6 +1,8 @@
+#ifndef _VERTEX_ARRAY_CLASS_HPP_
+#def _VERTEX_ARRAY_CLASS_HPP_
 namespace Renderer_API {
-class VertexArray {
 
+class VertexArray {
 public :
 enum AttribLayout { NONE, V, VC, VN, VCN };
 AttribLayout layout;
@@ -8,7 +10,7 @@ AttribLayout layout;
  VertexArray() : layout(AttribLayout::NONE) {}
 ~VertexArray() {}
 
-float vertex_array;
+std::vector<float> vertex_array;
 bool setLayout(Renderer_API::VertexArray::AttribLayout input_layout) : layout(input_layout) {}
 std::vector<float>* BuildVertexArray(std::vector<float>* position, std::vector<float>* color, std::vector<float>* normal);
 

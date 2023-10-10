98b2988f3a274eef844971854764105999a2a5e4
srikargridpro <147362350+srikargridpro@users.noreply.github.com>
2023-10-10 16:21:11 +0530

    Update Vertex_Array_Class.hpp

--- Renderer_API/Vertex_Array_Class.hpp
+++ Renderer_API/Vertex_Array_Class.hpp
@@ -2,6 +2,7 @@
 #define _VERTEX_ARRAY_CLASS_HPP_
 #include <cstdint>
 #include <vector>
+#include <iostream>
 
 namespace Renderer_API {
 class VertexArray {
@@ -50,7 +51,10 @@ Vertex curr_vertex;
 
 std::vector<float> vertex_array;
 bool setLayout(AttribLayout input_layout)  {  this->layout = input_layout ; return (input_layout > 0 ? true : false); }
+
 std::vector<float>* BuildVertexArray(std::vector<float>* position, std::vector<float>* color, std::vector<float>* normal);
+std::vector<float>* UpdateVertexArray(std::vector<float>* position, std::vector<float>* color, std::vector<float>* normal);
+
 bool push_back(Vertex vertex);
 
 
@@ -68,16 +72,18 @@ Vertex operator[](uint32_t index)
  {
    vertex.position[i] = this->vertex_array[index*stride_len + i];
    if(layout == 2 || layout == 4) 
-     vertex.color[i]  = this->vertex_array[index*stride_len + 3 + i]; // color offset = 3 
+   vertex.color[i]    = this->vertex_array[index*stride_len + 3 + i]; // color offset = 3 
    if(layout == 3)
-     vertex.normal[i] = this->vertex_array[index*stride_len + 3 + i]; // normal offset = 3
+   vertex.normal[i]   = this->vertex_array[index*stride_len + 3 + i]; // normal offset = 3
    if(layout == 4)
-     vertex.normal[i] = this->vertex_array[index*stride_len + 6 + i]; // normal offset = 6
+   vertex.normal[i]   = this->vertex_array[index*stride_len + 6 + i]; // normal offset = 6
  }
  
  vertex.update();
  return vertex;
 }
+
+
 
 }; // class VertexArray
 } // namespace Renderer_API

7d38139b346129cb1f5ec4f509dd04c13280fc04
srikargridpro <147362350+srikargridpro@users.noreply.github.com>
2023-10-10 12:09:52 +0530

    Update Vertex_Array_Class.hpp

--- Renderer_API/Vertex_Array_Class.hpp
+++ Renderer_API/Vertex_Array_Class.hpp
@@ -12,10 +12,25 @@ AttribLayout layout;
  VertexArray() : layout(AttribLayout::NONE) {}
 ~VertexArray() {}
 
-std::vector<float> vertex_array;
+struct Vertex {
+float position[3];
+float color[3];
+float normal[3];
+float* getPosition() { return this->position;}
+float* getColor() { return this->color;}
+float* getNormal() { return this->normal;}
+};
+
+Vertex operator[](int index)
+{
+ 
+}
+
 bool setLayout(AttribLayout input_layout)  {  this->layout = input_layout ; return (input_layout > 0 ? true : false); }
 std::vector<float>* BuildVertexArray(std::vector<float>* position, std::vector<float>* color, std::vector<float>* normal);
 
+private :
+std::vector<float> vertex_array;
 }; // class VertexArray
 } // namespace Renderer_API
 #endif  // _VERTEX_ARRAY_CLASS_HPP_

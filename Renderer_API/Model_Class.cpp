566a50aabf64d87ebd8ce38370e1ace7fb11e456
srikargridpro <147362350+srikargridpro@users.noreply.github.com>
2023-10-11 16:05:44 +0530

    Update Model_Class.cpp

--- Renderer_API/Model_Class.cpp
+++ Renderer_API/Model_Class.cpp
@@ -5,54 +5,55 @@
 
 namespace Renderer_API {
 
-void Model::setVisibilityFlag(Visibility_Flag flag)
+void Model::setVisibilityFlag(Visibility_Flag visibility_flag)
 {
-  
+  this->model_visibility = visibility_flag;
 }
 
 void Model::setPrimitive(PrimitiveType primitive_type, float primitive_thickness)
 {
-  
+  this->primitive_type = primitive_type;
+  this->primitive_thickness = primitive_thickness;
 }
 
 void Model::setPolygonMode(PolgonMode polygon_mode)
 {
-  
+  this->polygon_mode = polygon_mode;
 }
 
 void Model::setVertexAttribLayout(VertexArray::AttribLayout layout)
 {
-  
+  this->vertex_array.setLayout(layout);
 }
 
-void Model::setVertexAttribData(std::vector<float>* positon, std::vector<float>* color, std::vector<float>* normal)
+void Model::setVertexAttribData(std::vector<float>* positon = nullptr, std::vector<float>* color = nullptr, std::vector<float>* normal = nullptr)
 {
-  
+  this->vertex_array.BuildVertexArray(position, color, normal)
 }
 
-void Model::updateVertexAttribData(std::vector<float>* positon, std::vector<float>* color, std::vector<float>* normal)
+void Model::updateVertexAttribData(std::vector<float>* positon = nullptr, std::vector<float>* color = nullptr, std::vector<float>* normal = nullptr)
 {
-  
+  this->vertex_array.UpdateVertexArray(position, color, normal);
 }
 
 void Model::setIndexData(std::vector<uint32_t>& index_array)
 {
-  
+  this->index_array = index_array;
 }
 
 void Model::setShadeModel(ShadeModel shade_model)
 {
-  
+  this->shade_model = shade_model;
 }
 
 void Model::setModelMaterial(Material model_material)
 {
-  
+  this->ModelMaterial = model_material;
 }
 
 void Model::setLighting(bool Lighting_Switch_Flag)
 {
-  
+  this->lighting_flag =  Lighting_Switch_Flag ? 0 : 1;
 }
 
 void Model::render()

da12e660b4470b2fb74aa041d33363d4cdd8b376
srikargridpro <147362350+srikargridpro@users.noreply.github.com>
2023-10-11 15:25:14 +0530

    Update Model_Class.cpp

--- Renderer_API/Model_Class.cpp
+++ Renderer_API/Model_Class.cpp
@@ -3,11 +3,17 @@
 
 namespace Renderer_API {
 
-void Model::
-
-
-
-
+void setVisibilityFlag(Visibility_Flag flag)
+void setPrimitive(PrimitiveType primitive_type, float primitive_thickness);
+void setPolygonMode(PolgonMode polygon_mode);
+void setVertexAttribLayout(VertexArray::AttribLayout layout);
+void setVertexAttribData(std::vector<float>* positon, std::vector<float>* color, std::vector<float>* normal);
+void updateVertexAttribData(std::vector<float>* positon, std::vector<float>* color, std::vector<float>* normal);
+void setIndexData(std::vector<uint32_t>& index_array);
+void setShadeModel(ShadeModel shade_model);
+void setModelMaterial(Material model_material);
+void setLighting(bool Lighting_Switch_Flag);
+void render();
 
 }
 #endif

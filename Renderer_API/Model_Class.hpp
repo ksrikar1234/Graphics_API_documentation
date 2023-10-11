b04beb647fd7d3ebb781c7b70366a806da67b5dd
srikargridpro <147362350+srikargridpro@users.noreply.github.com>
2023-10-11 13:24:55 +0530

    Update Model_Class.hpp

--- Renderer_API/Model_Class.hpp
+++ Renderer_API/Model_Class.hpp
@@ -1,23 +1,55 @@
 #ifndef _MODEL_CLASS_HPP_
 #define _MODEL_CLASS_HPP_
-#define USE_HEADER_ONLY_IMPLEMENTATION
+
 #include "Vertex_Array_Class.hpp"
+#define USE_HEADER_ONLY_IMPLEMENTATION
+#include "Shader_Class.hpp"
+#undef USE_HEADER_ONLY_IMPLEMENTATION
 
 namespace Renderer_API {
 class Model {
 public :
 
+uint64_t UUID;
+
 uint32_t VBO, VAO;
-VertexArray vertex_array; 
+
+enum class PrimitiveType         {POINTS, LINES, LINE_STRIP, TRIANGLES, TRIANGLE_STRIP, QUADS, QUAD_STRIP};
+enum class ShadeModel            {FLAT , SMOOTH};
+enum class ColorSchema           {MONO, PER_PRIMITIVE, PER_VERTEX};
+enum class PolygonMode           {POINTS, LINES, FILL};
+enum class DisplayType           {NODisplay, DisplayHLR, DisplayShade, DisplayShadeHLR, DisplayLines, DisplayPoints}
+
+enum Visibility_Flag             {ENABLE, DISABLE};
+enum Material_Flag               {ENABLE, DISABLE};
+enum Lighting_Flag               {ENABLE, DISABLE};
+
+
+VertexArray vertex_array;
+VertexArray::AttribLayout layout;
+void setVertexAttribLayout(VertexArray::AttribLayout layout);
+void setVertexAttribData(std::vector<float>* positon, std::vector<float>* color, std::vector<float>* normal);
+void updateVertexAttribData(std::vector<float>* positon, std::vector<float>* color, std::vector<float>* normal);
+void setIndexData();
+
 Shader ModelShader;
+ShadeModel shade_model;
+void setShadeModel();
 
-
-
-
+struct Material
+{
+   Material_Flag material_flag
+   float color;
+   float shininess;
+   float ambient[3], diffuse[3] , specular[3];
 };
 
+Material ModelMaterial;
+void setMaterial(Material material);
+void setLighting(bool Lighting_Switch_Flag);
+void render();
 
-
+};
 
 } // namespace Renderer_API
 #endif // _MODEL_CLASS_HPP_

8b0ec4ce977dec634448a0e9f3e7da70e8d2b83d
srikargridpro <147362350+srikargridpro@users.noreply.github.com>
2023-10-13 11:36:44 +0530

    Update Shader_Sources.hpp

--- Renderer_API/shader_src/Shader_Sources.hpp
+++ Renderer_API/shader_src/Shader_Sources.hpp
@@ -1,8 +1,7 @@
-#ifndef _RENDERER_IN_BUILT_SHADER_SOURCES_
-#define _RENDERER_IN_BUILT_SHADER_SOURCES_
+#ifndef _RENDERER_API_IN_BUILT_SHADER_SOURCES_
+#define _RENDERER_API_IN_BUILT_SHADER_SOURCES_
 
 namespace Renderer_API {
-namespace ShaderSources{
 
 /*
      Valid Shader combinations
@@ -34,7 +33,7 @@ const char* vc_vertex_shader = R"(
     uniform mat4 projectionMatrix;
     
     uniform float mono_color_scheme;
-    uniform float model_color;
+    uniform vec3 model_color;
 
     void main()
     {    
@@ -57,7 +56,7 @@ const char* v_vertex_shader = R"(
     uniform mat4 projectionMatrix;
     
     uniform float mono_color_scheme;
-    uniform float model_color;
+    uniform vec3 model_color;
 
     void main()
     {    
@@ -102,9 +101,9 @@ uniform mat4 projectionMatrix;
     
 uniform vec3 lightPosition;
 
-uniform vertex_light_enabled
+uniform float vertex_light_enabled;
 uniform float mono_color_scheme;
-uniform float model_color;
+uniform vec3 model_color;
 
 void main()
 {
@@ -112,7 +111,7 @@ void main()
     // Transform vertex position and normal to world space
     vec4 worldPosition = modelMatrix * vec4(position, 1.0);
    
-    if(vertex_light_enabled)
+    if(vertex_light_enabled == 1.0f)
     {
       vec3 worldNormal = mat3(transpose(inverse(modelMatrix))) * normal;
 
@@ -163,10 +162,8 @@ uniform mat4 projectionMatrix;
     
 uniform vec3 lightPosition;
 
-uniform vertex_light_enabled;
-uniform float 
-;
-uniform float model_color;
+uniform float vertex_light_enabled;
+uniform vec3 model_color;
 
 void main()
 {
@@ -343,9 +340,105 @@ const char* OffScreenFragmentShaderSource = R"(
     }
 )";
 
-} // ShaderSources
-} // Renderer_API
+//----------------------------------------------------Blinn Phongs Lighting algorithm ------------------------------------------------------------------------------------+
+
+const char* OnScreenVertexShaderSource = R"(
+#version 330 core
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 color;
+layout(location = 2) in vec3 normal;
+
+out vec3 fragNormal;
+out vec3 fragPosition;
+out vec3 fragLightDir;
+out vec3 vertexColor;
+
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
+uniform vec3 lightPosition;
+
+void main()
+{
+
+    // Transform vertex position and normal to world space
+    vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+    vec3 worldNormal = mat3(transpose(inverse(modelMatrix))) * normal;
+
+    // Compute the light direction
+    fragLightDir = normalize(lightPosition - worldPosition.xyz);
+
+    // Pass data to the fragment shader
+    fragNormal = worldNormal;
+    fragPosition = worldPosition.xyz;
+     
+      vertexColor = color;
+     //  vertexColor = vec3(0.8f, 0.4f , 0.8f );
+    
+
+    // Transform the vertex position to clip space
+    gl_Position = projectionMatrix * viewMatrix * worldPosition;
+}
+)";
+
+//------------------------------------------On Screen rendering Fragment Shader----------------------------------------------------------------+
+
+const char* OnScreenFragmentShaderSource = R"(
+
+#version 330 core
+
+in vec3 fragNormal;
+in vec3 fragPosition;
+in vec3 fragLightDir;
+in vec3 vertexColor;
+
+
+out vec4 fragColor;
+
+uniform vec3 lightAmbient;
+uniform vec3 lightDiffuse;
+uniform vec3 lightSpecular;
+
+uniform vec3 materialAmbient;
+uniform vec3 materialDiffuse;
+uniform vec3 materialSpecular;
+uniform float materialShininess;
+
+
+void main()
+{  
+  
+// Compute the normal and light direction in fragment space
+vec3 normal = normalize(fragNormal);
+vec3 lightDir = normalize(fragLightDir);
+vec3 viewDir = normalize(-fragPosition);
+
+// Compute the diffuse color
+float diffuseFactor = max(dot(normal, lightDir), 0.0);
+vec3 diffuseColor = lightDiffuse * materialDiffuse * diffuseFactor;
+
+// Compute the half-angle vector (Blinn-Phong)
+vec3 halfDir = normalize(lightDir + viewDir);
+
+// Compute the specular color (Blinn-Phong)
+float specularFactor = pow(max(dot(normal, halfDir), 0.0), materialShininess);
+vec3 specularColor = lightSpecular * materialSpecular * specularFactor;
+
+// Compute the final color (ambient + diffuse + specular)
+vec3 ambientColor = lightAmbient * materialAmbient;
+vec3 finalColor = ambientColor + diffuseColor + specularColor;
+finalColor = finalColor * vertexColor;
+fragColor = vec4(finalColor, 1.0f);
 
 }
-#endif //  _RENDERER_IN_BUILT_SHADER_SOURCES_
 
+)";
+
+
+//} // ShaderSources
+} // Renderer_API
+
+
+#endif //  _RENDERER_IN_BUILT_SHADER_SOURCES_

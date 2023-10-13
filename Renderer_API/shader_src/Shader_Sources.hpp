3a2f17d34362467a567a4db6c83cacf9bbb63099
srikargridpro <147362350+srikargridpro@users.noreply.github.com>
2023-10-13 12:18:07 +0530

    Update Shader_Sources.hpp

--- Renderer_API/shader_src/Shader_Sources.hpp
+++ Renderer_API/shader_src/Shader_Sources.hpp
@@ -305,9 +305,9 @@ const char* simpleFragmentShaderSource = R"(
 const char* OffScreenVertexShaderSource = R"(
     #version 330 core
 
-    layout (location = 0) in vec3 VertexPos;
-    layout (location = 1) in vec3 VertexColor;
-    layout (location = 2) in vec3 VertexNormal;
+    layout(location = 0) in vec3 position;
+    layout(location = 1) in vec3 color;
+    layout(location = 2) in vec3 normal;
  
     out vec3 outColor;
   
